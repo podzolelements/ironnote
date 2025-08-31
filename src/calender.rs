5d94c940eddc54bc72e4224196bdbc06bc7e1459
podzolelements <podzolelements@mailbox.org>
2025-08-31 14:51:14 -0600

    clippy fixes

--- src/calender.rs
+++ src/calender.rs
@@ -14,9 +14,9 @@ pub enum CalenderMessage {
 
 #[derive(Clone, Copy, Debug)]
 pub enum Month {
-    LastMonth,
-    CurrentMonth,
-    NextMonth,
+    Last,
+    Current,
+    Next,
 }
 
 pub struct Calender {
@@ -80,38 +80,37 @@ impl Calender {
             start_offset = 7;
         }
 
-        let days_in_last_month: u32;
-        if active_datetime.month() == 1 {
-            days_in_last_month = 31;
+        let days_in_last_month = if active_datetime.month() == 1 {
+            31
         } else {
             let nd =
                 NaiveDate::from_ymd_opt(active_datetime.year(), active_datetime.month() - 1, 1)
                     .expect("bad date");
 
-            days_in_last_month = nd.num_days_in_month() as u32;
-        }
+            nd.num_days_in_month() as u32
+        };
         let mut cal_first_date = (days_in_last_month - start_offset) + 1;
 
         let mut current_day_addr = 0;
 
         for _day_last_month in 0..start_offset {
             self.day_list[current_day_addr] = cal_first_date;
-            self.month_mapping[current_day_addr] = Month::LastMonth;
+            self.month_mapping[current_day_addr] = Month::Last;
             current_day_addr += 1;
             cal_first_date += 1;
         }
 
         for day_in_month in 1..=(nd.num_days_in_month() as u32) {
             self.day_list[current_day_addr] = day_in_month;
-            self.month_mapping[current_day_addr] = Month::CurrentMonth;
+            self.month_mapping[current_day_addr] = Month::Current;
             current_day_addr += 1;
         }
 
-        let eom = current_day_addr.clone();
+        let eom = current_day_addr;
         let mut next_month_count = 1;
         for _day_next_month in eom..42 {
             self.day_list[current_day_addr] = next_month_count;
-            self.month_mapping[current_day_addr] = Month::NextMonth;
+            self.month_mapping[current_day_addr] = Month::Next;
             next_month_count += 1;
             current_day_addr += 1;
         }
@@ -125,7 +124,7 @@ impl Default for Calender {
     fn default() -> Self {
         Self {
             day_list: [0; 42],
-            month_mapping: [Month::LastMonth; 42],
+            month_mapping: [Month::Last; 42],
             month_text: String::new(),
             year_text: String::new(),
         }
