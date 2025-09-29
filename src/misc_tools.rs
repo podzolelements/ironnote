ef1d3c5488dad778f850c1b930a8751f9c2bc761
podzolelements <podzolelements@mailbox.org>
2025-09-29 16:07:18 -0600

    clicking on a search result jumps to the entry's day

--- src/misc_tools.rs
+++ src/misc_tools.rs
@@ -1,5 +1,7 @@
 // random tools and utilities that don't really fit anywhere in specific
 
+use chrono::{DateTime, Local, NaiveDate, TimeZone};
+
 /// returns true if all of the characters in the input string are the same character. returns true on an empty string
 pub fn chars_all_same_in_string(input: &str) -> bool {
     if let Some(first_char) = input.chars().next() {
@@ -29,4 +31,12 @@ pub fn first_whitespace_left(input: &str, starting_idx: usize) -> usize {
     }
 
     starting_idx - characters_moved
+}
+
+/// converts a string in the form of "YYYY-MM-DD" into a DateTime<Local>
+pub fn string_to_datetime(input: &str) -> DateTime<Local> {
+    let nd = NaiveDate::parse_from_str(input, "%Y-%m-%d").expect("couldn't parse date");
+    let ndt = nd.and_hms_opt(0, 0, 0).expect("couldn't create ndt");
+
+    Local.from_local_datetime(&ndt).unwrap()
 }
